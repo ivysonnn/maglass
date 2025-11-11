6c57fe96f24379bf054b778b2a0435765e159222
ivysonnn <ivysonlucas27@hotmail.com>
2025-11-11 15:52:14 -0300

    improved the install script

--- install.sh
+++ install.sh
@@ -1,10 +1,31 @@
 set -e
 
 if command -v pacman &> /dev/null; then
-    sudo pacman -S --needed --noconfirm raylib
+    dependencies="cmake grim raylib"
+    to_install=()
+    for dep in $dependencies; do
+        if ! pacman -Qs $dep &> /dev/null; then
+            to_install+=($dep)
+        fi
+    done
+
+    if [ ${#to_install[@]} -gt 0 ]; then
+        echo "The following dependencies are missing: ${to_install[*]}. Installing..."
+        sudo pacman -S --needed --noconfirm "${to_install[@]}"
+    fi
 else
-	echo "Unsupported distro."
-	echo "Please install 'raylib' manually using your distribution package manager."
+    for dep in cmake grim; do
+        if ! command -v $dep &> /dev/null; then
+            echo "Error: $dep is not installed." >&2
+            echo "Please install $dep and run this script again." >&2
+            exit 1
+        fi
+    done
+    if ! pkg-config --exists raylib &> /dev/null; then
+        echo "Error: raylib is not installed." >&2
+        echo "Please install 'raylib' (or 'libraylib-dev') manually using your distribution's package manager." >&2
+        exit 1
+    fi
 fi
 
 echo "Creating build folder..."
